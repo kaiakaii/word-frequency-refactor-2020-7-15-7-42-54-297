78d0758595cb1b2de0091368fdcc79fa5382c278
SUNSK <senkk512@163.com>
2020-07-27 15:44:52 +0800

    [WordFrequency][Sky] rename  w1 to firstWordInfo

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -34,7 +34,7 @@ public class WordFrequencyGame {
                 }
                 wordInfos = list;
 
-                wordInfos.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordInfos.sort((firstWordInfo, secondWordInfo) -> secondWordInfo.getWordCount() - firstWordInfo.getWordCount());
 
                 StringJoiner joiner = new StringJoiner(NEW_LINE);
                 for (WordInfo w : wordInfos) {
