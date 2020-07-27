87a99215342ee6359195e3d9339af4b3f9124367
SUNSK <senkk512@163.com>
2020-07-27 15:37:23 +0800

    [WordFrequency][Sky] rename  inputStr  to sentence

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -13,13 +13,13 @@ public class WordFrequencyGame {
     private static final String CALCULATE_ERROR = "Calculate Error";
     private static final String BLANK_SPACE = " ";
 
-    public String getResult(String inputStr) {
+    public String getResult(String sentence) {
 
-        if (inputStr.split(SPACE_PATTERN).length == 1) {
-            return inputStr + " 1";
+        if (sentence.split(SPACE_PATTERN).length == 1) {
+            return sentence + " 1";
         } else {
             try {
-                String[] arr = inputStr.split(SPACE_PATTERN);
+                String[] arr = sentence.split(SPACE_PATTERN);
 
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {
